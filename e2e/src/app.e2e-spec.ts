3ee19fc47cb16c07077c662a2ed61736bac33d20
amitkshirsagar13 <amit.kshirsagar.13@gmail.com>
2020-05-31 12:16:36 +0000

    first commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('white-board app is running!');
+    expect(page.getTitleText()).toEqual('whiteboard-ng app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'white-board'`, () => {
+  it(`should have as title 'whiteboard-ng'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('white-board');
+    expect(app.title).toEqual('whiteboard-ng');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('white-board app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('whiteboard-ng app is running!');
   });
 });
